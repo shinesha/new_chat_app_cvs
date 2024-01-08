08310bd2212b7a14b5f3622ddb4aad5d07ca768b
Gohil <viran.gohil@fujitsu.com>
2024-01-08 14:09:17 +0000

    added citation code so it uses azure key vault18

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -535,26 +535,26 @@ const Chat = () => {
         setIsCitationPanelOpen(true);
     };
 
-    const onViewSource = (citation: Citation) => {
-        if (citation.url) {
-            window.open(citation.url, "_blank");
-        }
-    };
-
-
     // const onViewSource = (citation: Citation) => {
-    //     try {
-    //         if (citation.url) {
-    //             // Append the key "123" as a query parameter named "myKey"
-    //             const urlWithKey = `${citation.url}?myKey=1UFqA2LbS+zhXJWQze+XnkSYD0MxqABv1vxz9cUqw9IDEnwh2jdv36gwjUiRr+4Ix9rXrDUUN2ae+AStj53dvQ==`;
-    
-    //             // Open the URL in a new window
-    //             window.open(urlWithKey, "_blank");
-    //         }
-    //     } catch (error) {
-    //         console.error("Error:", error);
+    //     if (citation.url) {
+    //         window.open(citation.url, "_blank");
     //     }
     // };
+
+
+    const onViewSource = (citation: Citation) => {
+        try {
+            if (citation.url) {
+                // Append the key "123" as a query parameter named "myKey"
+                const urlWithKey = `${citation.url}?sp=r&st=2024-01-08T13:36:21Z&se=2034-08-01T20:36:21Z&spr=https&sv=2022-11-02&sr=c&sig=7QFeyANbRkuQRAzk4yqKm%2FwfMHr0Kk%2BVXKzFVIM8Yss%3D`;
+    
+                // Open the URL in a new window
+                window.open(urlWithKey, "_blank");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
     
 
 
